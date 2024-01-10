8606339179a14968b2bb6075d3c936ac58a06078
Galvao <rafaeloliveira@febracis.com.br>
2024-01-10 00:37:22 -0300

    adjustment

--- src/presentation/vehicles/schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const vehicleSchema = Joi.object({
-    brand: Joi.string(),
-    plate: Joi.string().required(),
-    type: Joi.string().valid('car', 'motorcycle', 'truck'),
-});
-
-module.exports = vehicleSchema;
