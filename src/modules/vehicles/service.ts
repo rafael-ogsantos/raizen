353dc2f49e1c7e2f91f9194e791005421632a674
Galvao <rafaeloliveira@febracis.com.br>
2024-01-10 20:47:19 -0300

    service methods

---
+++ src/modules/vehicles/service.ts
@@ -0,0 +1,28 @@
+import * as repository from './repository';
+import { Vehicle, VehicleInput } from './schema';
+
+export const createVehicle = async (vehicle: VehicleInput): Promise<Vehicle> => {
+    const vehicleExists = await repository.getVehicleByPlate(vehicle.plate);
+    if (vehicleExists) {
+        throw new Error('A vehicle with this plate already exists');
+    }
+
+    return await repository.createVehicle(vehicle);
+}
+
+export const getVehicleByPlate = async (plate: string): Promise<Vehicle> => {
+    return await repository.getVehicleByPlate(plate);
+}
+
+export const updateVehicle = async (id: string, vehicle: Partial<Vehicle>): Promise<Vehicle> => {
+    return await repository.updateVehicle(id, vehicle);
+}
+
+export const deleteVehicle = async (id: string): Promise<void> => {
+    return await repository.deleteVehicle(id);
+}
+
+export const getVehicle = async (id: string): Promise<Vehicle> => {
+    return await repository.getVehicle(id);
+}
+
