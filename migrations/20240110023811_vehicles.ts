4344f05754884999ea271d3bb29a6abb5b332af7
Galvao <rafaeloliveira@febracis.com.br>
2024-01-11 12:08:21 -0300

    validate brand

--- migrations/20240110023811_vehicles.ts
+++ migrations/20240110023811_vehicles.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('vehicles', table => {
         table.string('id').primary();
-        table.string('brand');
+        table.enum('brand', ['XABLAU', 'VALIDVEHICLE']);
         table.string('plate').notNullable();
         table.enum('type', ['car', 'truck', 'motorcycle']).notNullable();
         table.dateTime('createdAt').notNullable();
